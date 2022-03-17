f1915c76b1569981f0f05f0189322ab6981eef31
DeltaTecs <delta-tecs@web.de>
2022-03-17 21:43:04 +0100

    fixes

--- update.bat
+++ update.bat
@@ -1,7 +1,7 @@
 git add chatters.json  challenges.json
 git commit -m "temp"
 git reset --hard
-git reset HEAD~
 git pull
+git reset HEAD~
 echo Update finished. Local changes overwritten. You may close this window.
 pause
