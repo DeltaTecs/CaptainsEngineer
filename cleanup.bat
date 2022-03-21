dda0b61985372115bc9ecc2e5de0bb17ad83c4b2
ccasimir <ccasimir@casimir.de>
2022-03-21 19:53:00 +0100

    final

--- cleanup.bat
+++ cleanup.bat
@@ -1,12 +1,7 @@
 xcopy CaptainsEngineer-main\* . /q /y /exclude:excludecp.txt
-pause
 xcopy temp\* . /q /h /i /c /k /e /r /y
-pause
 DEL /F /S /Q "update.zip"
-pause
 rmdir /q /s temp
-pause
 rmdir /q /s CaptainsEngineer-main
-pause
 powershell -Command "node bot.js"
 pause
--- update.bat
+++ update.bat
@@ -1,9 +1,7 @@
 powershell -Command "Invoke-WebRequest https://github.com/DeltaTecs/CaptainsEngineer/archive/refs/heads/main.zip -OutFile update.zip"
 powershell -Command "tar -xf update.zip"
 md temp
-pause
 xcopy challenges.json temp /q /r /y
 xcopy config.json temp /q /r /y
 xcopy chatters.json temp /q /r /y
-pause
 cleanup.bat
