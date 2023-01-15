fff89d62f10afe7291632135b7a7b5aae6ec8e97
DeltaTecs <martin.bach@stud.tu-darmstadt.de>
2023-01-15 16:41:49 +0100

    fix update

--- cleanup.bat
+++ cleanup.bat
@@ -1,4 +1,4 @@
-xcopy CaptainsEngineer-main\* . /q /y /exclude:excludecp.txt
+xcopy CaptainsEngineer-main\* . /s /e /q /y /exclude:excludecp.txt
 xcopy temp\* . /q /h /i /c /k /e /r /y
 DEL /F /S /Q "update.zip"
 rmdir /q /s temp
